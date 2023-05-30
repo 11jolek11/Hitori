d5a714a5286c904475697395a7f915fd8a2e67e6
11jolek11 <dabrowskiandrzej094@gmail.com>
2023-05-30 11:28:54 +0200

    cplex solution works

--- converter.py
+++ converter.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 
-
 def convert(board:np.ndarray):
     unique_numbers = np.unique(board)
     result = np.zeros([len(unique_numbers), *board.shape])
@@ -19,16 +18,17 @@ def convert(board:np.ndarray):
     # return result
 
 def writer(board:np.ndarray, dat_file: str="./task.dat"):
-    header = r"""
-    
-    """
     size_of_board = len(board)
     unique_values = len(np.unique(board))
     converted_board = convert(board)
-    with open(dat_file, "w"):
-        pass
-
-    
+    content = f"""
+    unique_values = {unique_values};
+    size_of_board = {size_of_board};
+    board_state = {converted_board};
+    """
+    print(content)
+    with open(dat_file, "w") as file:
+        file.write(content)
 
 if __name__ == "__main__":
     # test = np.array([[2, 2], [2, 3]])
@@ -52,12 +52,20 @@ if __name__ == "__main__":
     #-----------------------------------------------------------------------------
     # Initialize the problem data
     #-----------------------------------------------------------------------------
+    # Edge case. A solution is:
+    # 1 *
+    # 2 1
+    HITORI_PROBLEM_EDGE = (
+                        (1, 1),
+                        (2, 1),
+                    )
+
 
     # Problem 0 (for test). A solution is:
     #   * 2 *
     #   2 3 1
     #   * 1 *
-    HITORI_PROBLEM_0 = ( (2, 2, 1),
+    HITORI_PROBLEM_0 = ((2, 2, 1),
                         (2, 3, 1),
                         (1, 1, 1),
                     )
@@ -67,7 +75,7 @@ if __name__ == "__main__":
     #   * 1 * 3 5
     #   1 * 5 * 2
     #   5 4 3 2 1
-    HITORI_PROBLEM_1 = ( (2, 2, 1, 5, 3),
+    HITORI_PROBLEM_1 = ((2, 2, 1, 5, 3),
                         (2, 3, 1, 4, 5),
                         (1, 1, 1, 3, 5),
                         (1, 3, 5, 4, 2),
@@ -82,7 +90,7 @@ if __name__ == "__main__":
     #   * 5 6 7 * 1 8 *
     #   6 * 2 3 5 4 7 8
     #   8 7 1 4 * 3 * 6
-    HITORI_PROBLEM_2 = ( (4, 8, 1, 6, 3, 2, 5, 7),
+    HITORI_PROBLEM_2 = ((4, 8, 1, 6, 3, 2, 5, 7),
                         (3, 6, 7, 2, 1, 6, 5, 4),
                         (2, 3, 4, 8, 2, 8, 6, 1),
                         (4, 1, 6, 5, 7, 7, 3, 5),
@@ -93,7 +101,7 @@ if __name__ == "__main__":
                     )
 
     # Problem 3, solution to discover !
-    HITORI_PROBLEM_3 = ( ( 2,  5,  6,  3,  8, 10,  7,  4, 13,  6, 14, 15,  9,  4,  1),
+    HITORI_PROBLEM_3 = (( 2,  5,  6,  3,  8, 10,  7,  4, 13,  6, 14, 15,  9,  4,  1),
                         ( 3,  1,  7, 12,  8,  4, 10,  4,  4, 11,  5, 13,  4,  9,  2),
                         ( 4, 14, 10, 10, 14,  5, 11,  1,  6,  2,  7, 11, 13, 15, 12),
                         ( 5, 10,  2,  5, 13,  3,  8,  5,  9,  7,  4, 10,  6, 10,  2),
@@ -110,8 +118,4 @@ if __name__ == "__main__":
                         ( 6,  9,  1, 13,  6,  4, 12,  7, 14,  4,  2,  1,  3,  8, 12)
                     )
 
-
-    # print(len(convert(HITORI_PROBLEM_1)))
-    print(convert(np.asarray(HITORI_PROBLEM_1)))
-    # writer(np.asarray(HITORI_PROBLEM_1))
-
+    writer(np.asarray(HITORI_PROBLEM_2))
